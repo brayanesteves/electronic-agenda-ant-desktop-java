1f69ebd2780f6dba171f8e86e5c6c5195bd66220
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-24 16:16:28 -0400

    GUI - Electronic Agenda (Main) [24/10/2023]

---
+++ src/electronic/agenda/ant/desktop/java/gui/Main.java
@@ -0,0 +1,248 @@
+package electronic.agenda.ant.desktop.java.gui;
+
+/**
+ *
+ * @author Brayan Esteves
+ */
+public class Main extends javax.swing.JFrame {
+
+    public Main() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelContent = new javax.swing.JPanel();
+        labelTitle = new javax.swing.JLabel();
+        separatorTop = new javax.swing.JSeparator();
+        separatorBottom = new javax.swing.JSeparator();
+        panelForm = new javax.swing.JPanel();
+        labelDocumentoIdentification = new javax.swing.JLabel();
+        labelName = new javax.swing.JLabel();
+        labelLastname = new javax.swing.JLabel();
+        textfieldDocumentIdentification = new javax.swing.JTextField();
+        textfieldName = new javax.swing.JTextField();
+        textfieldLastname = new javax.swing.JTextField();
+        labelAddress = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        textfieldAddress = new javax.swing.JTextField();
+        textfieldPhone = new javax.swing.JTextField();
+        textfieldDateOfBirth = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        buttonLast = new javax.swing.JButton();
+        buttonSave = new javax.swing.JButton();
+        buttonNext = new javax.swing.JButton();
+        labelIndex = new javax.swing.JLabel();
+        textfieldIndex = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        labelTitle.setFont(new java.awt.Font("Lato", 1, 23)); // NOI18N
+        labelTitle.setText("Electronic Agenda");
+
+        labelDocumentoIdentification.setText("Document Identification:");
+
+        labelName.setText("Name:");
+
+        labelLastname.setText("Lastname:");
+
+        labelAddress.setText("Address:");
+
+        jLabel5.setText("Phone:");
+
+        jLabel6.setText("Date of birth:");
+
+        textfieldAddress.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textfieldAddressActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panelFormLayout = new javax.swing.GroupLayout(panelForm);
+        panelForm.setLayout(panelFormLayout);
+        panelFormLayout.setHorizontalGroup(
+            panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelFormLayout.createSequentialGroup()
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelFormLayout.createSequentialGroup()
+                        .addGap(93, 93, 93)
+                        .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(labelName, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(labelLastname, javax.swing.GroupLayout.Alignment.TRAILING)))
+                    .addGroup(panelFormLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(labelDocumentoIdentification)))
+                .addGap(18, 18, 18)
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(textfieldDocumentIdentification)
+                    .addComponent(textfieldName)
+                    .addComponent(textfieldLastname, javax.swing.GroupLayout.DEFAULT_SIZE, 215, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6)
+                    .addComponent(labelAddress))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(textfieldDateOfBirth, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textfieldPhone, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textfieldAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+        panelFormLayout.setVerticalGroup(
+            panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelFormLayout.createSequentialGroup()
+                .addGap(57, 57, 57)
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(textfieldDocumentIdentification, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelDocumentoIdentification)
+                    .addComponent(labelAddress)
+                    .addComponent(textfieldAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(textfieldName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelName)
+                    .addComponent(jLabel5)
+                    .addComponent(textfieldPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(panelFormLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(textfieldLastname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelLastname)
+                    .addComponent(jLabel6)
+                    .addComponent(textfieldDateOfBirth, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 23, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout panelContentLayout = new javax.swing.GroupLayout(panelContent);
+        panelContent.setLayout(panelContentLayout);
+        panelContentLayout.setHorizontalGroup(
+            panelContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelContentLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(separatorTop, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(separatorBottom, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(panelForm, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelContentLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(labelTitle)
+                .addGap(343, 343, 343))
+        );
+        panelContentLayout.setVerticalGroup(
+            panelContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelContentLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(labelTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(separatorTop, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelForm, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(separatorBottom, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        buttonLast.setText("<<");
+
+        buttonSave.setText("Save");
+
+        buttonNext.setText(">>");
+
+        labelIndex.setText("Index");
+
+        textfieldIndex.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        textfieldIndex.setText("0");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(202, 202, 202)
+                        .addComponent(buttonLast)
+                        .addGap(110, 110, 110)
+                        .addComponent(buttonSave)
+                        .addGap(117, 117, 117)
+                        .addComponent(buttonNext))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(363, 363, 363)
+                        .addComponent(labelIndex)
+                        .addGap(18, 18, 18)
+                        .addComponent(textfieldIndex, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonLast)
+                    .addComponent(buttonSave)
+                    .addComponent(buttonNext))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(labelIndex)
+                    .addComponent(textfieldIndex, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelContent, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panelContent, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void textfieldAddressActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textfieldAddressActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textfieldAddressActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonLast;
+    private javax.swing.JButton buttonNext;
+    private javax.swing.JButton buttonSave;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel labelAddress;
+    private javax.swing.JLabel labelDocumentoIdentification;
+    private javax.swing.JLabel labelIndex;
+    private javax.swing.JLabel labelLastname;
+    private javax.swing.JLabel labelName;
+    private javax.swing.JLabel labelTitle;
+    private javax.swing.JPanel panelContent;
+    private javax.swing.JPanel panelForm;
+    private javax.swing.JSeparator separatorBottom;
+    private javax.swing.JSeparator separatorTop;
+    private javax.swing.JTextField textfieldAddress;
+    private javax.swing.JTextField textfieldDateOfBirth;
+    private javax.swing.JTextField textfieldDocumentIdentification;
+    private javax.swing.JTextField textfieldIndex;
+    private javax.swing.JTextField textfieldLastname;
+    private javax.swing.JTextField textfieldName;
+    private javax.swing.JTextField textfieldPhone;
+    // End of variables declaration//GEN-END:variables
+}
