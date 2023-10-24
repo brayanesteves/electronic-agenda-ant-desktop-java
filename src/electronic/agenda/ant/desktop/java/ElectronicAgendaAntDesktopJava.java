d154fb0f99bf6a43d7c2d7151176decccee24545
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-24 15:55:46 -0400

    Assigment [24/10/2023]

---
+++ src/electronic/agenda/ant/desktop/java/ElectronicAgendaAntDesktopJava.java
@@ -0,0 +1,71 @@
+
+package electronic.agenda.ant.desktop.java;
+
+/**
+ * @author Brayan Esteves
+ * Challenge:
+ * [ENGLISH]:
+ * Exercise: Electronic Agenda.
+ * A program is needed to manage contacts in an electronic address book.
+ * a. In the address book, the following data must be allowed to be entered:
+ *    national identity number, name, last name, date of birth, phone number and
+ *    address (Each in its corresponding TextField).
+ * b. The data of 10 people needs to be stored. For this, it is proposed to use
+ *    1 vector to store the values of each of the fields (Considering that different
+ *    data types may exist), where each index will indicate the data of a certain
+ *    person in all vectors. For example: 
+ *    1) National ID Vector: 36051447, 34825697, 14899589 [...]
+ *    2) Names Vector: Luisina, Guillermo, Esteban [...]
+ *    3) Last Names Vector: de Paula, Oink, Cugliari [...]
+ * c. The program must allow values to be loaded into a given position of the
+ *    vector (By clicking the load button) and traverse the vector finding the
+ *    loaded data. For example:
+ * Window description:
+ * 1) Title: Electronic Agenda (Label).
+ * 2) Row 0:
+ *    2.1) National ID (Label and Textfield) and Address (Label and Textfield).
+ * 3) Row 1:
+ *    3.1) Name (Label and Textfield) and Phone (Label and Textfield).
+ * 4) Row 2:
+ *    4.1) Last Name (Label and Textfield) and Date of Birth (Label and Textfield).
+ * 5) Back Navigation Button [Back] (Button).
+ * 6) Action Button [Save] (Button). 7) Next Navigation Button [Next] (Button). 8) Index (Label and Textfield).
+ * [=======]
+ * [SPANISH]:
+ * Ejercicio: Agenda Electrónica.
+ * Se necesita un programa para gestionar los contactos de una agenda electrónica.
+ * a. En la agenda, se debe permitir ingresar los siguientes datos: dni, nombre,
+ *    apellido, fecha de nacimiento, teléfono y dirección (Cada uno en su correspondiente
+ *    TextField).
+ * b. Se necesitan almacenar los datos de 10 personas. Para ello se propone la
+ *    utilización de 1 vector para almacenar los valores de cada uno de los campos
+ *    (Teniendo en cuenta que pueden existir distintos tipos de datos), en donde
+ *    cada índice indicará los datos de una determinada persona en todos los vectores.
+ *    Por ejemplo:
+ *    1) Vector DNI: 36051447, 34825697, 14899589 [...]
+ *    2) Vector Nombres: Luisina, Guillermo, Esteban [...]
+ *    3) Vector Apellidos: de Paula, Oink, Cugliari [...]
+ * c. El programa debe permitir cargar valores en una determinada posición del
+ *    vector (Al hacer click en el botón cargar) y recorrer el vector encontrando
+ *    los datos cargados. Por ejemplo:
+ * Descripción de la ventana:
+ * 1) Titulo: Agenda Electrónica (Label).
+ * 2) Fila 0:
+ *    2.1) DNI (Label y Textfield) y Dirección (Label y Textfield).
+ * 3) Fila 1:
+ *    3.1) Nombre (Label y Textfield) y Telefono (Label y Textfield).
+ * 4) Fila 2:
+ *    4.1) Apellido (Label y Textfield) y Fecha de Nacimiento (Label y Textfield).
+ * 5) Botón de navegación [Atras] (Button).
+ * 6) Botón de acción [Guardar] (Button).
+ * 7) Botón de navegación [Siguiente] (Button).
+ * 8) Índice (Label y Textfield).
+ */
+public class ElectronicAgendaAntDesktopJava {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
